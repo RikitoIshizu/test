c61f5b3115bec99ccba2b1b96ded7c3babc0a013
Rikito Ishizu <riktioishizu@RikitonoMacBook-Pro.local>
2022-04-02 13:30:29 +0900

    eslint解消、リファクタリング

---
+++ components/atoms/Meta/index.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Head from 'next/head'
+
+type MetaProps = {
+	title: string
+	description: string
+}
+
+const Meta = (props: MetaProps ) => {
+	return (
+		<Head>
+			<title>{props.title}</title>
+			<meta name="description" content={props.description} />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+	)
+}
+
+export default Meta
